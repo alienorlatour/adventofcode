d194a699599c1331a3d78b0964ad83f5cd7fdd51
Aliénor Latour <alienor.latour@gmail.com>
2022-12-04 16:11:13 +0100

    More dirty work on day 1

--- 2022/dec01/dec_test.go
+++ 2022/dec01/dec_test.go
@@ -6,8 +6,9 @@ import (
 	"github.com/stretchr/testify/assert"
 )
 
-func TestDec25_Solve(t *testing.T) {
+func TestDec_Solve(t *testing.T) {
 	s, err := New("testdata/in.txt").Solve()
 	assert.NoError(t, err)
-	assert.Contains(t, s, "24000")
+	// assert.Contains(t, s, "24000")
+	assert.Contains(t, s, "45000")
 }
